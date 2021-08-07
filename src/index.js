a159166e3ad52cc5d2fad299253a64add298aeb8
luisgm21 <gonzy.morelli@gmail.com>
2021-08-07 15:48:33 -0300

    configuraciones finales

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const path = require('path')
 const app = express()
 
 // settings
@@ -7,14 +8,17 @@ app.set('port', process.env.PORT || 3000)
 // Middlewares
 app.use(morgan('dev'))
 app.use(express.json())
+
+// Routes
+app.use('/api/event', require('./routes/event.routes'))
+// Static files
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use((req, res) => {
   res.status(404).json({
     error: 'Not found'
   })
 })
-// Routes
-app.use('/api/event', require('./routes/event.routes'))
-
 // starting server
 app.listen(app.get('port'), () => {
   console.log(`server conectado en puerto ${app.get('port')}`)
